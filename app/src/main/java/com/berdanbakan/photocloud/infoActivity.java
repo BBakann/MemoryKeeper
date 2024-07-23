4561fad483e6470d73ccb7686733b54f50a2d773
Berdan Bakan <berdanbakan2@gmail.com>
2024-07-23 17:02:35 +0300

    Project created and InfoActivity XML design completed

---
+++ app/src/main/java/com/berdanbakan/photocloud/infoActivity.java
@@ -0,0 +1,34 @@
+package com.berdanbakan.photocloud;
+
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class infoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_info);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+
+    public void selectImage(View view) {
+    }
+
+    public void save(View view) {
+    }
+
+    public void back(View view) {
+    }
+}
